e462f976fb1b3d1ebab300aa7e1e3ff04a99f29b
ppazosp <pablopazosp3@gmail.com>
2025-02-22 01:53:57 +0100

    NavBar v1.0 added

--- app/src/main/java/ochat/wearendar/MainActivity.kt
+++ app/src/main/java/ochat/wearendar/MainActivity.kt
@@ -1,16 +1,12 @@
-package ochat.wearendar
+package ochat.wearendar.ui
 
 import android.os.Bundle
 import androidx.activity.ComponentActivity
 import androidx.activity.compose.setContent
 import androidx.activity.enableEdgeToEdge
 import androidx.compose.foundation.layout.fillMaxSize
-import androidx.compose.foundation.layout.padding
 import androidx.compose.material3.Scaffold
-import androidx.compose.material3.Text
-import androidx.compose.runtime.Composable
 import androidx.compose.ui.Modifier
-import androidx.compose.ui.tooling.preview.Preview
 import ochat.wearendar.ui.theme.WearendarTheme
 
 class MainActivity : ComponentActivity() {
@@ -19,29 +15,8 @@ class MainActivity : ComponentActivity() {
         enableEdgeToEdge()
         setContent {
             WearendarTheme {
-                Scaffold(modifier = Modifier.fillMaxSize()) { innerPadding ->
-                    Greeting(
-                        name = "Android",
-                        modifier = Modifier.padding(innerPadding)
-                    )
-                }
+                NavGraph()
             }
         }
-    }
-}
-
-@Composable
-fun Greeting(name: String, modifier: Modifier = Modifier) {
-    Text(
-        text = "Hello $name!",
-        modifier = modifier
-    )
-}
-
-@Preview(showBackground = true)
-@Composable
-fun GreetingPreview() {
-    WearendarTheme {
-        Greeting("Android")
     }
 }
